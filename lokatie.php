4927d881899e104da9eaac50fe3f78150625e00f
I316906 <l.daalmeijer@student.fontys.nl>
2016-04-26 11:06:47 +0200

    fouten in links aangepast

--- lokatie.php
+++ lokatie.php
@@ -28,24 +28,9 @@
                         Daimler App
                     </a>
                 </li>
-                <li>
-                    <a href="dashboard.php">Dashboard</a>
-                </li>
-                <li>
-                    <a href="tabel.php">Tabel</a>
-                </li>
-                <li>
-                    <a href="lokatie.php">Lokatie</a>
-                </li>
-                <li>
-                    <a href="instellingen.php">Instellingen</a>
-                </li>
-                <li>
-                    <a href="help.php">Help</a>
-                </li>
-                <li>
-                    <a href="loguit.php">Log Uit</a>
-                </li>
+                <?php
+                include "menu.php";
+                ?>
             </ul>
         </div>
         <!-- /#sidebar-wrapper -->
---
+++ menu.php
@@ -0,0 +1,18 @@
+<li>
+                    <a href="index.php">Dashboard</a>
+                </li>
+                <li>
+                    <a href="tabel.php">Tabel</a>
+                </li>
+                <li>
+                    <a href="lokatie.php">Lokatie</a>
+                </li>
+                <li>
+                    <a href="instellingen.php">Instellingen</a>
+                </li>
+                <li>
+                    <a href="help.php">Help</a>
+                </li>
+                <li>
+                    <a href="loguit.php">Log Uit</a>
+                </li>
