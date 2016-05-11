5c1b8bbb846dc0cad036a8aec530dc851b3b400b
Robyn <r.holskens@student.fontys.nl>
2016-05-11 21:14:55 +0200

    login.php aangemaakt en een form toegevoegd

---
+++ login.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+    <link href='https://fonts.googleapis.com/css?family=PT+Sans:400,400italic,700' rel='stylesheet' type='text/css'>
+
+	<title>Dashboard | Daimler</title>
+
+    <!-- Custom CSS -->
+    <link href="css/simple-sidebar.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <script type="text/javascript" src="js/modules.js"></script>
+
+</head>
+<body>
+	<div class="login-page">
+  		<div class="form">
+
+			<form class="login-form">
+		      <input type="text" placeholder="Gebruikersnaam"/>
+		      <input type="password" placeholder="Wachtwoord"/>
+		      <button>login</button>
+		      <p class="message">Nog niet geregistreerd? <a href="#">Maak een account.</a></p>
+		    </form>
+
+		</div>
+	</div>
+
+</body>
+</html>
