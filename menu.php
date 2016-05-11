2e728334a166a7ddc31af503c452e0039bebc343
Robyn <r.holskens@student.fontys.nl>
2016-05-11 20:57:13 +0200

    Definieer de navigatie

--- menu.php
+++ menu.php
@@ -1,18 +1,20 @@
-<li>
-    <a href="index.php"><span class="fa fa-dashboard" style="color: #999999; margin-right:5px;"></span>Dashboard</a>
-</li>
-<li>
-    <a href="tabel.php"><span class="fa fa-server" style="color: #999999; margin-right:5px;"></span>Tabel</a>
-</li>
-<li>
-    <a href="lokatie.php"><span class="fa fa-map-marker" style="color: #999999; margin-right:10px;"></span>Locatie</a>
-</li>
-<li>
-    <a href="instellingen.php"><span class="fa fa-wrench" style="color: #999999; margin-right:5px;"></span>Instellingen</a>
-</li>
-<li>
-    <a href="help.php"><span class="fa fa-comments-o" style="color: #999999; margin-right:5px;"></span>Help</a>
-</li>
-<li>
-    <a href="loguit.php"><span class="fa fa-power-off" style="color: #999999; margin-right:5px;"></span>Log Uit</a>
-</li>
+<div class="nav">
+	<li>
+	    <a href="index.php"><span class="fa fa-dashboard"></span>Dashboard</a>
+	</li>
+	<li>
+	    <a href="tabel.php"><span class="fa fa-server"></span>Tabel</a>
+	</li>
+	<li>
+	    <a href="lokatie.php"><span class="fa fa-map-marker"></span>Locatie</a>
+	</li>
+	<li>
+	    <a href="instellingen.php"><span class="fa fa-wrench"></span>Instellingen</a>
+	</li>
+	<li>
+	    <a href="help.php"><span class="fa fa-comments-o"></span>Help</a>
+	</li>
+	<li>
+	    <a href="loguit.php"><span class="fa fa-power-off" style="color: #999999; margin-right:5px;"></span>Log Uit</a>
+	</li>
+</div>
