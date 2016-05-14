bb1ec42f58d9bf2cddb1fcb8ff28efcc1291731c
Robyn <r.holskens@student.fontys.nl>
2016-05-14 20:11:41 +0200

    connect.php gewijzigd om loginpagina te maken

--- connect.php
+++ connect.php
@@ -1,14 +1,22 @@
 <?php
-$servername = "192.168.0.42";
+// <<<<<<< Updated upstream
+// $servername = "192.168.0.42";
+// $username = "root";
+// $password = "admin";
+// =======
+$servername = "localhost";
 $username = "root";
-$password = "admin";
+$password = "root";
+$dbname = "daimler";
+
+// >>>>>>> Stashed changes
 
 // Create connection
-$conn = mysqli_connect($servername, $username, $password);
+$con = mysqli_connect($servername, $username, $password, $dbname);
 
 // Check connection
-if (!$conn) {
+if (!$con) {
     die("Connection failed: " . mysqli_connect_error());
 }
-echo "Connected successfully";
+// echo "Connected successfully";
 ?>
