90098816ebfb4faca128ec2841458ed55874ea36
Samantha Verbakel <smmverbakel@gmail.com>
2016-05-23 11:00:40 +0200

    location geupdate

--- lokatie.php
+++ lokatie.php
@@ -63,8 +63,8 @@
         
               function initMap() {
                 var map = new google.maps.Map(document.getElementById('map'), {
-                  center: {lat: -34.397, lng: 150.644},
-                  zoom: 6
+                  center: {lat: 52.23, lng: 4.554},
+                  zoom: 8
                 });
                 var infoWindow = new google.maps.InfoWindow({map: map});
         
